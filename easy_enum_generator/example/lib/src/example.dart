aae701199726f9335c1d4d9347d0d82a83fa9fff
Chris D <chris@venngo.com>
2021-08-31 21:57:20 -0400

    documented example

--- easy_enum_generator/example/lib/src/example.dart
+++ easy_enum_generator/example/lib/src/example.dart
@@ -2,12 +2,15 @@ import 'package:easy_enum/easy_enum.dart';
 
 part 'example.g.dart';
 
+///default enumeration settings
 @EasyEnum()
 enum SimpleState { play, paused, stopped }
 
+///custom method names to retrieve values
 @EasyEnum(toValue: 'theValue', toEnum: 'theRating')
 enum SomeRating { none, dislike, like }
 
+///A simple test class
 class Test {
   final String name;
   final SimpleState state;
